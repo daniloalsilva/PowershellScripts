e259382d3d384a64a849a4d4c01506513ba35c04
Danilo Silva <danilo.silva@locaweb.com.br>
2015-01-13 17:18:19 -0200

    adding Monitor-HttpRequest.ps1

---
+++ IIS/Monitor-HttpRequest.ps1
@@ -0,0 +1,99 @@
+<#  
+.SYNOPSIS  
+    Gets the currently web requests of IIS from logs to retrieve http requests 
+
+.DESCRIPTION
+    The Monitor-WebRequest.ps1 uses PSModule WebAdministration to retrieve configurated log path.
+    The Script will keep all logfiles with LastWrite equal actual day openned and "Tail" it.
+    Data will be collected every 1000msec and a summary will be displayed on console during data collection.
+    After passed specified time collecting data, an GridView will pop-up with data collection result.
+    
+    Usage: Monitor-WebRequest [options]:
+        -secondsToMonitor  - time in seconds to keep executing and collectng data
+
+.NOTES  
+    File Name      : Monitor-HttpRequest.ps1
+    Author         : Danilo Silva - daniloalsilva@gmail.com
+    Prerequisite   : PowerShell V2 or greater.
+                   : Windows Server 2008 or greater
+                   : PSModule WebAdministration
+    Copyright 2015 - Danilo Silva
+
+.EXAMPLE
+    Monitor-HttpRequest.ps1 -secondsToMonitor 60
+    
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [int]$secondsToMonitor
+)
+
+Import-Module WebAdministration
+
+$sites = ls IIS:\Sites
+$logFiles = foreach ($site in $sites){
+    $dirlog = ls $site.logFile.directory -ErrorAction SilentlyContinue
+    if ($dirlog -ne $null){
+        $logs = ls $dirlog.FullName | ? { $_.LastWriteTime.Date -eq (Get-Date).Date }
+    }
+    $logs.FullName
+}
+
+$reader = @{}
+$lastMaxOffset = @{}
+
+foreach($log in ($logFiles | select -Unique)){
+
+    $reader[$log] = new-object System.IO.StreamReader(New-Object IO.FileStream($log, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [IO.FileShare]::ReadWrite))
+    $lastMaxOffset[$log] = $reader[$log].BaseStream.Length
+}
+
+$elapsed = [System.Diagnostics.Stopwatch]::StartNew()
+
+$output = while ($elapsed.Elapsed.TotalSeconds -lt $secondsToMonitor){
+
+    foreach($log in ($logFiles | select -Unique)){
+
+        if ($reader[$log].BaseStream.Length -eq $lastMaxOffset[$log]) {
+            continue;
+        }
+
+        $reader[$log].BaseStream.Seek($lastMaxOffset[$log], [System.IO.SeekOrigin]::Begin) | out-null
+
+        $line = ""
+        while (($line = $reader[$log].ReadLine()) -ne $null) {
+            $lineSplitted = $line.Split()
+            
+            new-object psobject -Property @{
+                "date" = $lineSplitted[0]
+                "time" = $lineSplitted[1]
+                "s-sitename" = $lineSplitted[2]
+                "s-computername" = $lineSplitted[3]
+                "s-ip" = $lineSplitted[4]
+                "cs-method" = $lineSplitted[5]
+                "cs-uri-stem" = $lineSplitted[6]
+                "cs-uri-query" = $lineSplitted[7]
+                "s-port" = $lineSplitted[8]
+                "cs-username" = $lineSplitted[9]
+                "c-ip" = $lineSplitted[10]
+                "cs-version" = $lineSplitted[11]
+                "cs(User-Agent)" = $lineSplitted[12]
+                #"cs(Cookie)" = $lineSplitted[13]
+                "cs(Referer)" = $lineSplitted[14]
+                "cs-host" = $lineSplitted[15]
+                "sc-status" = $lineSplitted[16]
+                "sc-substatus" = $lineSplitted[17]
+                "sc-win32-status" = $lineSplitted[18]
+                "sc-bytes" = $lineSplitted[19]
+                "cs-bytes" = $lineSplitted[20]
+                "time-taken"= $lineSplitted[21]
+            }
+        }
+        
+        $lastMaxOffset[$log] = $reader[$log].BaseStream.Position
+    }
+    sleep -m 1000
+}
+
+$output |  Out-GridView
